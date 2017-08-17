74a8d651c801ef9ab41f777aeb0698de7df094fc
Prabhalaa2 <30726851+Prabhalaa2@users.noreply.github.com>
2017-08-17 11:27:06 -0400

    Initial commit

---
+++ Paint/Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Squarebox = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Red
+        Me.Button1.Location = New System.Drawing.Point(3, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(24, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.White
+        Me.Button2.BackgroundImage = CType(resources.GetObject("Button2.BackgroundImage"), System.Drawing.Image)
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Button2.Location = New System.Drawing.Point(3, 90)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(89, 63)
+        Me.Button2.TabIndex = 1
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Black
+        Me.Button3.Location = New System.Drawing.Point(34, 61)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(23, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.Fuchsia
+        Me.Button4.Location = New System.Drawing.Point(63, 32)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(24, 23)
+        Me.Button4.TabIndex = 3
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Blue
+        Me.Button5.Location = New System.Drawing.Point(33, 32)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(24, 23)
+        Me.Button5.TabIndex = 4
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.Lime
+        Me.Button6.Location = New System.Drawing.Point(3, 32)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(24, 23)
+        Me.Button6.TabIndex = 5
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.Yellow
+        Me.Button7.Location = New System.Drawing.Point(63, 3)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(24, 23)
+        Me.Button7.TabIndex = 6
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button8.Location = New System.Drawing.Point(33, 3)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(24, 23)
+        Me.Button8.TabIndex = 7
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Controls.Add(Me.Button4)
+        Me.Panel1.Controls.Add(Me.Button5)
+        Me.Panel1.Controls.Add(Me.Button6)
+        Me.Panel1.Controls.Add(Me.Button7)
+        Me.Panel1.Controls.Add(Me.Button8)
+        Me.Panel1.Location = New System.Drawing.Point(999, 204)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(92, 156)
+        Me.Panel1.TabIndex = 9
+        '
+        'Squarebox
+        '
+        Me.Squarebox.Location = New System.Drawing.Point(1038, 12)
+        Me.Squarebox.Name = "Squarebox"
+        Me.Squarebox.Size = New System.Drawing.Size(53, 49)
+        Me.Squarebox.TabIndex = 10
+        Me.Squarebox.Text = "Square"
+        Me.Squarebox.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.ClientSize = New System.Drawing.Size(1103, 442)
+        Me.Controls.Add(Me.Squarebox)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Squarebox As Button
+End Class
---
+++ Paint/Form1.vb
@@ -0,0 +1,124 @@
+﻿Public Class Form1
+    Dim click As String
+    Dim mybrush As New SolidBrush(Color.Black)
+    Dim thickness As Integer = 10
+    Dim draw As System.Drawing.Graphics = Me.CreateGraphics
+    Dim rightclick As Integer
+    Dim square As Integer
+    Dim rightx As Integer
+    Dim righty As Integer
+    Dim leftx As Integer
+    Dim lefty As Integer
+
+
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Panel1.Visible = True
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        Dim draw As System.Drawing.Graphics
+        draw = Me.CreateGraphics
+
+        'draw.FillRectangle(Brushes.Red, 100, 100, 200, 200)
+        'draw.FillRectangle(Brushes.Blue, 100, 315, 200, 230)
+        'draw.FillRectangle(Brushes.Green, 325, 100, 225, 200)
+        'draw.FillRectangle(Brushes.Yellow, 325, 315, 225, 230)
+
+
+
+
+
+    End Sub
+
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+
+
+
+        Dim xPos As Integer = Me.MousePosition.X - Me.Location.X - 15
+        Dim yPos As Integer = Me.MousePosition.Y - Me.Location.Y - 40
+
+
+
+        Label1.Text = xPos
+        Label2.Text = yPos
+
+        If click >= 1 Then
+            draw.FillEllipse(mybrush, xPos, yPos, thickness, thickness)
+        End If
+
+        If rightclick = 1 Then
+            rightx = xPos
+            righty = yPos
+
+        End If
+
+        If click = 1 Then
+            leftx = xPos
+            lefty = yPos
+
+        End If
+
+
+
+        'draw.FillEllipse(Brushes.Black, xPos, yPos, 10, 10)
+    End Sub
+
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
+        click = 1
+        If e.Button = MouseButtons.Right Then
+            rightclick = 1
+        End If
+
+    End Sub
+
+    Private Sub Form1_MouseUp(sender As Object, e As MouseEventArgs) Handles Me.MouseUp
+        click = 0
+        rightclick = 0
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        mybrush.Color = Color.Red
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        mybrush.Color = Color.Orange
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        mybrush.Color = Color.Yellow
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        mybrush.Color = Color.Green
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        mybrush.Color = Color.Blue
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        mybrush.Color = Color.Purple
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        mybrush.Color = Color.Black
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        mybrush.Color = Color.White
+    End Sub
+
+    Private Sub Form1_MouseWheel(sender As Object, e As MouseEventArgs) Handles Me.MouseWheel
+        If e.Delta > 0 Then
+            thickness = thickness + 2
+        Else
+            thickness = thickness - 2
+        End If
+    End Sub
+
+    Private Sub Squarebox_Click(sender As Object, e As EventArgs) Handles Squarebox.Click
+        draw.DrawRectangle(New Pen(mybrush.Color, thickness), leftx, lefty, rightx, righty)
+    End Sub
+End Class
